fda40841ec1b81a39774e800c75222674161a140
brent crystal <brentrcrystal@gmail.com>
2026-02-12 07:32:06 -0500

    Adds MudBlazor Configuration. Removes sample weather project assets.

--- src/CrystalFinance.Ui/Program.cs
+++ src/CrystalFinance.Ui/Program.cs
@@ -1,12 +1,17 @@
 using CrystalFinance.Ui;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using CrystalFinance.Ui.Models;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => 
 {
