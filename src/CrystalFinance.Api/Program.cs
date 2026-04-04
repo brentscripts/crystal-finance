cf6f315a6ea8ae985150d30bf61a4557c9d32467
brent crystal <brentrcrystal@gmail.com>
2026-04-04 09:45:06 -0400

    Added data base default connection string to api appsettings and configured program.cs. Started CrystalFinanceController. Saving work and resuming on dedicated branch.

--- src/CrystalFinance.Api/Program.cs
+++ src/CrystalFinance.Api/Program.cs
@@ -1,12 +1,19 @@
 using CrystalFinance.Api.Startup;
+using CrystalFinanceLibrary.Data;
 using CrystalFinanceLibrary.Logic;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
 builder.AddDependencies();
 builder.AddAuthentication();
 builder.Services.AddCorsPolicy(builder.Configuration);
 builder.Services.AddScoped<TransactionImportService>();
+builder.Services.AddScoped<IMySqlDataService, MySqlData>(sp =>
+    new MySqlData(connectionString));
+
 var app = builder.Build();
 
 app.UseOpenApi();
