7f3ea198490a3b80ed9679ffb2122366beba0233
brent crystal <brentrcrystal@gmail.com>
2026-04-25 08:53:25 -0400

    Revert "Removes MudBlazor library. Setup bootstrap5 for manual custom component creation."
    This reverts commit e85fa54b045feeaba68b5bd33e75f27d0a6cf199.

--- src/CrystalFinance.Ui/Program.cs
+++ src/CrystalFinance.Ui/Program.cs
@@ -4,10 +4,15 @@ using CrystalFinance.Ui.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => 
 {
